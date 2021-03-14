9bcd56dea5decc6ef5002d6c0870cfbe4234f7c6
Alexandre Sabri <alexandre.sabri@etu.u-paris.fr>
2021-03-14 18:59:10 +0100

    rédaction de la classe des prefixes.

---
+++ Prefixes.java
@@ -0,0 +1,18 @@
+public enum Prefixes {
+	 DIFF 
+	,MESS
+	,ACKM
+	,LAST 
+	,OLDM
+	,ENDM
+	,REGI  
+	,REOK 
+	,RENO 
+	,RUOK 
+	,IMOK 
+	,LIST  
+	,LINB 
+	,ITEM ;
+	@Override 
+	public String toString(){ return this.name();}
+	}
