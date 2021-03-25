2b94a3bc5fcd9191be42075c06001821822513d4
Alexandre Sabri <alexandre.sabri@etu.u-paris.fr>
2021-03-25 10:11:25 +0100

    Rédaction de la classe Holder quasiment terminée, ainsi que sa documentation

--- Holder.java
+++ Holder.java
@@ -1,29 +1,46 @@
+import java.util.concurrent;
+
 public class Holder implements Iterator<Message> {
-	// modifier la classe message pour stocker si un message a été envoyé ou pas ?
-	// ou rajouter un tableau ici ? 
-	// penser à modifier la docu ! 
+	// FIXME penser à modifier la docu ! 
 	public Holder(){};
 	public static final int HOLDSZ = 999;
-
-	private int nextpos=0;// de quel indice dans le tableau sera récupéré le prochain message à envoyer
-	private int memorypos=0; // 
-	private boolean alreadySentBeyond=true; // si est à vrai, il n'y a plus rien à envoyer pour le moment
-	private Message mssgholder [] = new Message[HOLDSZ];
-	private Message msgghistory [] = new Message[HOLDSZ];
+	private ConcurrentLinkedDeque<Message> toSendQueue= new ConcurrentLinkedDeque();
+	int toSendOccupation=0;
+	int nextMessageNumber=0;
+	private ConcurrentLinkedDeque<Message> HistoryQueue= new ConcurrentLinkedDeque();
+	int historyOccupation =0;
+	
+	
+	public void add(Message toadd) throws NullPointerException{
+		if (toadd == null){throw new NullPointerException();}
+		if (tosendOccupation< HOLDSZ ){
+			this.toSendQueue.add(toadd);
+			toSendOccupation++;
+		}else{
+			this.toSendQueue.removeLast();
+			this.toSendQueue.add(toadd);
+			}
+	}
 	@Override
-	synchronized boolean 	hasNext() { return ! alreadySentBeyond;
-		}
+	public synchronized boolean hasNext() { 
+		return toSendOccupation <= 0 ? false:true;
+	}
+		
 	@Override
 	public  synchronized String next() throws NoSuchElementException {
-		//FIXME
-		// renvoyer le  toString du message après avoir ajouté son numéro
-		if(alreadySentBeyond){throw new NoSuchElementException();}
-			//récup message et adjonction chiffre compteur
-		String toret = this.nextpos.toString()+" "+this.mssgholder[this.nextpos].toString();
+		Message temp = toSendQueue.removeLast(); // lance NSEException si queue vide
+		this.toSendOccupation --;
+		String toret = this.nextMessageNumber.+temp.toString();
+		this.nextMessageNumber=(this.nextMessage  +1 )% HOLDSZ;
 		
-		this.nextpos = (this.nextpos+1)%this.HOLDSZ; // préparation pour le prochain next
-		if (this.nextpos == ths.memorypos){this.}
-		return toret;}
-	 public synchronized String[] retrieveHistory(int howmany){//FIXME a faire
-		 };
+		//si occupation maximale est atteinte on enlève le plus ancien élément et on en ajoute un nouveau
+		if(this.historyOccupation>=HOLDSZ){this.History.removeLast();this.HistoryQueue.add(temp);}
+		else{this.History.add(temp); this.historyOccupation++;}
+		
+		return toret;
 	}
+		
+	 public synchronized String[] retrieveHistory(int howmany){
+		
+	};
+}
