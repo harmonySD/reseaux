e2c05245154a7caa8c2be7c913b712c4319c4e73
Harmony Simon-Duchatel <sd.harmony@yahoo.fr>
2021-03-26 11:09:58 +0100

    envoyer Message tcp au diffusuer

--- utilisateur1.c
+++ utilisateur1.c
@@ -22,6 +22,37 @@ typedef struct{
     char port[4];
     char ip[15];
 }diffu;
+
+typedef struct{
+    int *sock;
+    char *id;
+}thread_arg;
+
+
+
+char *verif_lenght(char *str, int size){
+    char *id=str; 
+    int s=strlen(str);
+    //si la taille de l'id n'est pas egal a 8
+    if(s < size){
+        //on rajoute des # a l'id
+        for(int i = s-1; i < size-1; i++){
+           strcat(id,"#");
+        } 
+    }
+    //si la taille est plus grande que 8
+    else if (s > size){
+        //on tronque l'id a 8 caracteres
+        char *id2 =NULL;
+        id2=malloc(size);
+        for(int i =0; i<size; i++){
+            id2[i]=id[i];
+       }
+       id=id2;
+    }
+    return id;
+
+}
 /*diffu connection_gestionnaire(char *argv){ 
     int p=atoi(argv);
     struct sockaddr_in adress_sock;
@@ -47,33 +78,63 @@ typedef struct{
     }
 }*/
 
+
 void *sendMessage(void *sock_desc) {
-    int so=*((int *) sock_desc);
+    thread_arg *foo =(thread_arg*)sock_desc;
+    int so=*foo->sock;
+    char *id=foo->id;
+   
+    //int so=*((int *)sock_desc);
     //Send some data
     while (1) {
-        char message[2000];
+        char message[4];
         printf("%s","> ");
         scanf("%[^\n]%*c", message);
         fflush(stdin);
 
-        if(send(so, message, strlen(message), 0)<0){
-            puts("error ! not send");
+        if(strstr(message,"MESS")){
+            char messAenv[SIZE_MESS];
+            printf("%s","entree votre message d'au plus 140 characteres: ");
+            scanf("%[^\n]%*c", messAenv);
+            fflush(stdin);
+            char *m=verif_lenght(messAenv,SIZE_MESS);
+            char mess[4+SIZE_MESS+SIZE_ID+2];
+            strcpy(mess,message);
+            strcat(mess," ");
+            strcat(mess,foo->id);//id ne marche pas :(
+            strcat(mess," ");
+            strcat(mess,m);
+            send(so,mess,SIZE_ID+4+2+SIZE_MESS,0);
+
+
+            
         }
+        //regarder si MESS ou LAST 
+        //si MESS 
+        //r
+        //scaanf ragrder taille 
+        //strcpy MEss scan id etc 
+        //envoie 
+        
+    //     if(send(so, message, SIZE_MESS, 0)<0){
+    //        puts("error ! not send");
+    //    }
     }
 }
 
-void connection_diffuseur(char *port, char *ip, char *id){
+void connection_diffuseur(char *port1, char *ip1, char *port2, char *ip2, char *id){
+    printf("id ici : %s\n",id);
     int sock=socket(PF_INET,SOCK_DGRAM,0);
     int ok=1;
     int r=setsockopt(sock,SOL_SOCKET,SO_REUSEPORT,&ok,sizeof(ok));
     struct sockaddr_in address_sock;
     address_sock.sin_family=AF_INET;
-    address_sock.sin_port=htons(3434);//remplacer par port 
+    address_sock.sin_port=htons(atoi(port1));//3434 
     address_sock.sin_addr.s_addr=htonl(INADDR_ANY);
     r=bind(sock,(struct sockaddr *)&address_sock,sizeof(struct sockaddr_in));
     
     struct ip_mreq mreq;
-    mreq.imr_multiaddr.s_addr=inet_addr("225.1.2.4");//remplacer par ip
+    mreq.imr_multiaddr.s_addr=inet_addr(ip1);//"225.1.2.4"
     mreq.imr_interface.s_addr=htonl(INADDR_ANY);
 
     r=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
@@ -86,45 +147,31 @@ void connection_diffuseur(char *port, char *ip, char *id){
     if(r==0){    
         struct sockaddr_in adress_sock2;
         adress_sock2.sin_family = AF_INET;
-        adress_sock2.sin_port = htons(5757);
-        inet_aton("127.0.0.1",&adress_sock2.sin_addr);
+        adress_sock2.sin_port = htons(5757);//5757
+        inet_aton("127.0.0.1",&adress_sock2.sin_addr);//"127.0.0.1"
   
         int descr=socket(PF_INET,SOCK_STREAM,0);
         int r2=connect(descr,(struct sockaddr *)&adress_sock2,
                 sizeof(struct sockaddr_in));
+
         if(r2!=-1){
             char *mess="SALUT!\n";
             send(descr,mess,strlen(mess),0);
             pthread_t thread;
-            pthread_create(&thread,NULL,sendMessage,&descr );
+            thread_arg t;
+            t.id=id;
+            printf("id dans struct %s\n",t.id);
+            t.sock=&descr;
+
+            pthread_create(&thread,NULL,sendMessage,&t);
+        }else{
+            printf("rater");
         }
-        while(1){//while recv !=0 ?
-            //recevoir et peut etre envoyer mais comment faire si on veut envoyer un seule mess
-            //et continuer a recevoir le diffu ?
-            char mess[4]; //MESS
-            /*if (fgets(mess,4,stdin)!=NULL){
-                if(strcmp(mess,"MESS")){
-                    //envoie message choisit au hasard
-                    // verifier bien recu ACKM apres 
-                }
-                else if(strstr(mess,"LAST")){
-                    //envoie mess (normalement contient LAST 6) par exemple 
-                    //boucle sur nombre de mess etc 
-                }
-            }*/
+        while(1){
             char mess_recu[SIZE_MESS+4+8+4+3+1]; //+3 pour les espaces +1 pour '\0'
             int rec=recvfrom(sock,mess_recu,SIZE_MESS+4+8+4+3,0,(struct sockaddr *)&emet,&a);
             mess_recu[rec]='\0';
             printf("Message recu :%s\n",mess_recu);
-
-
-       
-  
-
-            // //keep communicating with server
-            // pthread_t send_thread;
-            // pthread_create(&send_thread, NULL, sendMessage, (void *) sock);
-
         }
 
     }
@@ -134,36 +181,13 @@ void connection_diffuseur(char *port, char *ip, char *id){
 
 
 
-char *verif_id(char *str){
-    char *id=str; 
-    int s=strlen(str);
-    //si la taille de l'id n'est pas egal a 8
-    if(s < SIZE_ID){
-        //on rajoute des # a l'id
-        for(int i = s-1; i < SIZE_ID-1; i++){
-           strcat(id,"#");
-        } 
-    }
-    //si la taille est plus grande que 8
-    else if (s > SIZE_ID){
-        //on tronque l'id a 8 caracteres
-        char *id2 =NULL;
-        id2=malloc(SIZE_ID);
-        for(int i =0; i<SIZE_ID; i++){
-            id2[i]=id[i];
-       }
-       id=id2;
-    }
-    return id;
-
-}
 
 int main(int argc, char**argv){
     if(argc != 2){
         printf("Erreur il faut fournir un numero de port puis un pseudo ! ");
         return 0;
     }
-    char *id=verif_id(argv[1]);
+    char *id=verif_lenght(argv[1],SIZE_ID);
     //printf("id : %s",id);
 
     //connection gestionnaire en mode TCP qui rempli une struture pour sauvegarde 
@@ -172,7 +196,7 @@ int main(int argc, char**argv){
 
     //diffu diffuseur=connection_gestionnaire(argv[1]);
     printf("id %s",id);
-    connection_diffuseur("3434","127.0.0.1",id);//port et  addresse issue de la structure  PAS SURE 
+    connection_diffuseur("3434","225.1.2.4","5757","127.0.0.1",id);//port et  addresse issue de la structure  PAS SURE 
 
 
 
