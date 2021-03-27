589336ae72ceb9140242db56db88ed28d4f35ec4
Alexandre Sabri <alexandre.sabri@etu.u-paris.fr>
2021-03-27 14:12:11 +0100

    Classe message et Holder terminées, compilent sans warning prêtes à partir en test, docu pour ces classes rédigée.

--- Holder.java
+++ Holder.java
@@ -1,65 +1,73 @@
-import java.util.concurrent;
+import java.util.concurrent.*;
+import java.util.Iterator;
+import java.util.*;
+public class Holder {
 
-public class Holder implements Iterator<Message> {
-	// FIXME penser à modifier la docu ! 
-	public Holder(){};
-	public static final int HOLDSZ = 999;
-	private ConcurrentLinkedDeque<Message> toSendQueue= new ConcurrentLinkedDeque();
-	int toSendOccupation=0;
-	int nextMessageNumber=0;
-	private Message [] HistoryQueue= new Message[HOLDSZ];
-	int historyOccupation =0;
+
+	public static final int HOLDSZ = 999; // nb maximal de messages que contient notre holder
+	//si un nouveau arrive, on écrase le plus ancien non envoyé
+	private ConcurrentLinkedDeque<Message> toSendQueue= new ConcurrentLinkedDeque<Message>();
+	private int toSendOccupation=0; // indique le nombre actuel de messages en attente d'envoi
+	private int nextMessageNumber=0;//indique le numéro (Modulo HOLDSZ) qu'aura le prochain message 
+	//utile pour l'envoi et pour le stockage dans l'historique ainsi que l'accès depuis l'historique
+	private Message [] HistoryQueue= new Message[HOLDSZ];// historique des derniers messages récupérés via next 
+	private int historyOccupation =0;// taille actuelle de l'historique, vas jusqu'à HOLDSZ puis chaque nouveau message écrase le plus ancien,
+	// grâce à l'utilisation de nextMessageNumber
+	
+	public Holder(){};// constructeur
 	
 	public synchronized void add(Message toadd) throws NullPointerException{
 		/**Ajoute un Message à la liste des messages à envoyer
 		 * lance NullPointerException si l'argument est null  **/
 		if (toadd == null){throw new NullPointerException();}
-		if (tosendOccupation< HOLDSZ ){ // si la queue n'est pas pleine
+		if (toSendOccupation< HOLDSZ ){ // si la queue n'est pas pleine
 			this.toSendQueue.add(toadd);
 			toSendOccupation++; // on augmente le compteur
-		}else{ // si la queue est pleine on supprimme le plus ancien non envoyé
+		}else{ // si la queue est pleine on supprime le plus ancien non envoyé
 			this.toSendQueue.removeLast();
 			this.toSendQueue.add(toadd);
 		}// pas d'augmentation compteur, liste déjà pleine !
 	}
 	
-	@Override
+
 	public synchronized boolean hasNext() { 
 		return toSendOccupation < 1 ? false:true;
 	}
 		
-	@Override
 	public  synchronized String next() throws NoSuchElementException { // renvoit le prochain message à envoyer
 		Message temp = toSendQueue.removeLast(); // lance NSEException si queue vide
 		this.toSendOccupation --; //maj occupation file à envoyer
-		String toret = this.nextMessageNumber.toString()+" "+temp.toString();// ajout du numéro du message pour envoi
+		String toret = Integer.toString(this.nextMessageNumber)+" "+temp.toString();// ajout du numéro du message pour envoi
 		if (this.historyOccupation< HOLDSZ){this.historyOccupation++;}
 		this.HistoryQueue[this.nextMessageNumber]=temp;
-		this.nextMessageNumber=(this.nextMessage  +1 )%HOLDSZ;
+		this.nextMessageNumber=(this.nextMessageNumber  +1 )%HOLDSZ;
 		return toret;
 	}
 		
-	public synchronized String[] retrieveHistory(int howmany){
+	public synchronized String[] retrieveHistory(int howmany){ // /!\ renvoit un tableau de String avec un null si historique vide !
+		// fonction de récupération de l'historique des messages, du plus récent à la position 0 au plus ancien  
 		String [] toret;
 		if (howmany> this.historyOccupation){
+			//tableau n'est pas plein et/ou trop de mssages demandés
 			toret = new String[this.historyOccupation];
 			for (int i = this.historyOccupation-1;i>-1;i--){
-				toret[this.historyOccupation-i]=this.HistoryQueue[i];
+				toret[this.historyOccupation-1-i]=Integer.toString(i)+" "+this.HistoryQueue[i].toString();
+				//les messages sont rangés dans toret du plus récent au plus ancien
 			}
 		}
 		else if (howmany<=this.nextMessageNumber-1){
+			// si pas trop de messages demandés ET on peut
+			//juste remonter le tableau de l'historique bêtement sans avoir à faire de saut
 		toret = new String[howmany];
-			for (int i = this.howmany-1;i>-1;i--){
-				toret[i]=this.HistoryQueue[this.nextMessageNumber-i];
+			for (int i = howmany-1;i>-1;i--){
+				toret[howmany-1-i]=Integer.toString((this.nextMessageNumber-1-i))+" "+this.HistoryQueue[this.nextMessageNumber-1-i].toString();
+			}
+		}else{// howmany> this.nextMessagenumber-1
+		toret = new String[howmany];
+			for (int i = howmany-1;i>-1;i--){
+				toret[howmany-1-i]=Integer.toString(this.nextMessageNumber-1-i%HOLDSZ)+" "+this.HistoryQueue[this.nextMessageNumber-1-i%HOLDSZ].toString();
 			}
 		}
-		else{// howmany> this.nextMessagenumber-1
-		toret = new String[howmany];
-			for (int i = this.historyOccupation;i>-1;i--){
-				toret[i]=this.HistoryQueue[this.nextMessageNumber-i];
-			}
-		}
-
 			return toret;
 	}
 }
--- Message.java
+++ Message.java
@@ -1,7 +1,7 @@
 public class Message {
 	// tailles fixée des données / nom utilisateur
-	public static final idsz=8;
-	public static final datasz=140;
+	public static final int idsz=8;
+	public static final int datasz=140;
 	
 	private final String id ;
 	private final String data  ;
@@ -10,12 +10,12 @@ public class Message {
 		// stockage id de l'émetteur du message
 		if (userid.length() != Message.idsz ){
 			if (userid.length() > Message.idsz){this.id = userid.substring(0,Message.idsz);}
-			else{this.id = userid+"#".repeat( Message.idsz - userid.length );}
+			else{this.id = userid+"#".repeat( Message.idsz - userid.length() );}
 			}else {this.id=userid;}
 		//stockage du message
 		if (message.length() != Message.datasz ){
 			if (message.length() > Message.datasz){this.data = message.substring(0,Message.datasz);}
-			else{this.data = message+"#".repeat( Message.datasz - message.length );}
+			else{this.data = message+"#".repeat( Message.datasz - message.length() );}
 			}else {this.data=message;}
 	}
 	
